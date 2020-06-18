d5cb12a2c83ac2ad586ff972567934970897d1f5
Noah Heck <noah@noahheck.com>
2020-06-18 13:08:16 -0600

    Add description og tag

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -14,6 +14,7 @@ $pricingRouteActive = Route::is('pricing') ? 'active' : '';
     <meta property="og:title" content="Office Forge">
     <meta property="og:type" content="website">
     <meta property="og:image" content="https://officeforge.net/images/of_logo_500.png">
+    <meta property="og:description" content="At Office Forge, we believe nobody knows your business as well as you do. Our goal is to give you software that works the way you work.">
 
     <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
 
